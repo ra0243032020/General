834e0bde33e8d3d9c7ddbdb68c74a07fc2b65d5e
ra0243032020 <69596926+ra0243032020@users.noreply.github.com>
2020-08-29 00:14:49 -0300

    PRIMER EJERCICIO SOLO

---
+++ MasterGeneral/src/main/java/com/unab/clase02/mastergeneral/MainClass.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.clase02.mastergeneral;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe tu Edad:");
+        int Edad = sc.nextInt();
+
+        System.out.println("Escribe tu Nombre:");
+        String Nombre = sc.next();
+
+        System.out.println("Escribe tu Sexo:");
+        String Sexo = sc.next();
+
+        System.out.println("Escribe tu Peso:");
+        float Peso = sc.nextFloat();
+
+        System.out.println("Escribe tu Altura:");
+        float Altura = sc.nextFloat();
+
+        Persona persona1 = new Persona(Nombre, Sexo, Peso, Altura, Edad);
+
+        Persona persona2 = new Persona(persona1.getNombre(), persona1.getSexo(), persona1.getEdad());
+
+        Persona persona3 = new Persona();
+        persona3.setNombre(Nombre);
+        persona3.setEdad(Edad);
+        persona3.setAltura(Altura);
+        persona3.setPeso(Peso);
+        persona3.setSexo(Sexo);
+        
+        System.out.println(persona1.esMayorDeEdad());
+        System.out.println(persona1.persona()); 
+       
+        System.out.println(persona2.esMayorDeEdad()); 
+        System.out.println(persona2.toString());
+    
+        System.out.println(persona3.esMayorDeEdad()); 
+        System.out.println(persona3.persona()); 
+    }
+
+}
---
+++ MasterGeneral/src/main/java/com/unab/clase02/mastergeneral/Persona.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.clase02.mastergeneral;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class Persona {
+
+    private String Nombre, Sexo;
+    private float Peso, Altura;
+    private int Edad;
+
+    public Persona() {
+
+    }
+
+    public Persona(String Nombre, String Sexo, int Edad) {
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;
+        this.Edad = Edad;
+    }
+
+    public Persona(String Nombre, String Sexo, float Peso, float Altura, int Edad) {
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;
+        this.Peso = Peso;
+        this.Altura = Altura;
+        this.Edad = Edad;
+    }
+
+    public String persona() {
+        return "Persona{" + "Nombre=" + Nombre + ", Sexo=" + Sexo + ", Peso=" + Peso + ", Altura=" + Altura + ", Edad=" + Edad + '}';
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "Nombre=" + Nombre + ", Sexo=" + Sexo + ", Edad=" + Edad + '}';
+    }
+
+    
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public float getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(float Peso) {
+        this.Peso = Peso;
+    }
+
+    public float getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(float Altura) {
+        this.Altura = Altura;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    String esMayorDeEdad() {
+            int Edad = this.Edad;
+        if (Edad >= 18) {
+            return "Eres meyor de edad";
+        } else {
+            return "Eres Menor de edas";
+
+        }
+
+    }
+}
